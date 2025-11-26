d0ab3a291578d925de215b26b65e27259c981fe8
tungBKDN <102210138@sv1.dut.udn.vn>
2025-11-26 16:08:28 +0700

    feat: done labelling

--- src/llm/pseudograph_relabelling_llm.py
+++ src/llm/pseudograph_relabelling_llm.py
@@ -1,55 +1,65 @@
+from html import entities
 import os
+import sys
 from google import genai
 from google.genai import types
 from typing import Union
 
+from prompt_toolkit import prompt
+
+
 class PseudoGraphRelabellingLLM:
     # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-    def __init__(self):
-        self.client = genai.Client(
-            api_key=os.getenv('PSEUDOGRAPH_RELABELLING_API_KEY'))
+    def __init__(self, model_name: str = "gemini-flash-latest", key = None):
+        if key == None:
+            key = os.getenv('PSEUDOGRAPH_RELABELLING_API_KEY')
+        self.CLIENT = genai.Client(api_key=key)
+        self.MODEL = model_name
+        self.CONFIG = types.GenerateContentConfig(
+            temperature=0.3,
+            thinking_config=types.ThinkingConfig(thinking_budget=8000),
+            system_instruction=[
+                types.Part.from_text(text="""You are a tool to generate triplet representation from claim data, entities, and evidence. With the given information, you have to generate the correct triplet representation balancing claim data and evidence. If there are any relations with prefix of ~, it's a reverse relation (not the negation)."""),
+            ],
+        )
 
-    def submit(self, claim_data: str, entities: Union[str, list], evidence: Union[str, dict], max_tokens: int = 32) -> str:
+    def generate_prompt(self, claim: str, claim_data: dict) -> str:
+        entities: list = claim_data.get("Entity_set", [])
+        evidence: dict = claim_data.get("Evidence", {})
 
-        if isinstance(entities, list):
-            entities = str(entities)
-        if isinstance(evidence, dict):
-            evidence = str(evidence)
+        prompt = (
+            f"Given the following JSON claim data:\n"
+            f"\"{claim}\"\n\n"
+            f"Given the following entities as an array:\n"
+            f"{str(entities)}\n\n"
+            f"Given the following evidences, each key-value is a pair of entity and its relationships:\n"
+            f"{str(evidence)}\n\n"
+            f"You are now have to find the correct triplet representation of the claim in the format, "
+            f"balancing claim data and evidence:\n"
+            f"<e>HEAD</e> || RELATION || <e>TAIL</e>\n\n"
+            f"You can skip if there is some issues with the equivalent evidence. "
+            f"If there are any implicit information in the claim data, use the unknown_i, "
+            f"where is an integer starting from 0. And some abundant relationships can be ignored based on the main context of the claim. And must choose the correct relationships if you are pooling from, like keeping the same timeline, etc.\n\n"
+            f"For example:\n"
+            f"Claim data: \"The author of 'Romeo and Juliet' is from a country that is in Europe.\"\n"
+            f"Entities: [\"Romeo and Juliet\", \"Europe\"]\n"
+            f"Evidence: {{\n"
+            f"    \"Romeo and Juliet\": [\"written_by\"],\n"
+            f"    \"Europe\": [\"continent_of\"]\n"
+            f"}}\n\n"
+            f"Correct triplet: <e>Romeo and Juliet</e> || written_by || <e>unknown_0</e>; "
+            f"<e>unknown_0</e> || continent_of || <e>Europe</e>\n"
+        )
+        return prompt
 
+    def submit(self, claim: str, claim_data: dict) -> str:
         # Tạo prompt đúng định dạng trong paper ClaimPKG
-        prompt = f"""
-                Given the following JSON claim data:
-                \"{claim_data}\"
+        prompt = self.generate_prompt(claim, claim_data)
 
-                Given the following entities as an array:
-                {entities}
-
-                Given the following evidences, each key-value is a pair of enitiy and its relationships:
-                {evidence}
-
-                You are now have to find the correct triplet representation of the claim in the format, balancing claim data and evidence:
-                <e>HEAD</e> || RELATION || <e>TAIL</e>
-
-                You can skip if there is some issues with the the equivalent evidence. If there are any implicit information in the claim data, use the unknown_i, where is an integer starting from 0.
-
-                For example:
-                Claim data: "The author of 'Romeo and Juliet' is from a country that is in Europe."
-                Entities: ["Romeo and Juliet", "Europe"]
-                Evidence: {
-                    "Romeo and Juliet": ["written_by"],
-                    "Europe": ["continent_of"]
-                }
-
-                Correct triplet: <e>Romeo and Juliet</e> || written_by || <e>unknown_0</e>; <e>unknown_0</e> || continent_of || <e>Europe</e>
-                """
-        response = self.client.models.generate_content(
-            model="gemini-2.0-flash",
+        response = self.CLIENT.models.generate_content(
+            model=self.MODEL,
             contents=prompt,
-            config=types.GenerateContentConfig(
-                temperature=0.3,
-                max_output_tokens=max_tokens,
-                system_instruction="You are a tool to generate triplet representation from claim data, entities, and evidence. With the given information, you have to generate the correct triplet representation balancing claim data and evidence. If there are any relations with prefix of ~, it's a reverse relation (not the negation).",
-            )
+            config=self.CONFIG,
         )
 
         if not response or not response.text:
